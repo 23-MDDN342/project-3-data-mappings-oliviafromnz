1688b3ec08aea0d2ec9aec091075999c786ec323
oliviafromnz <100629335+oliviafromnz@users.noreply.github.com>
2023-05-25 14:13:57 +0200

    Linking up and playing with nose

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=11118352&assignment_repo_type=AssignmentRepo)
 ## 2023 MDDN342 Assignment 3: Data Mappings
 
-REPLACE ALL TEXT IN THIS FILE
+Have linked up eyes and nose location (averaged) as well as made it so that when the nose is turning right then the nose has more depth with an inner layer. 
 
-This README should be used to document your design.
+Next will play with the mouth - odeally want to do these: 
+// if mouth is closed then squish the mouth and get rid of inner circle
+// if mouth is smiling with teeth then do shocked face squished
+// if half teeth then shoked face
+
+We will see - finding it a struggle to manipulate the circs knowing that everytime I refresh its random due to the noise function used....
+
+
--- face.js
+++ face.js
@@ -7,13 +7,13 @@
 var DEBUG_MODE = true;
 
 // this can be used to set the number of sliders to show
-var NUM_SLIDERS = 3;
+var NUM_SLIDERS = 7;
 
 // other variables can be in here too
 // here's some examples for colors used
 
 
-const stroke_color = [95, 52, 8];
+//const stroke_color = [95, 52, 8];
 
 // example of a global function
 // given a segment, this returns the average point [x, y]
@@ -29,18 +29,26 @@ function segment_average(segment) {
 }
 
 // This where you define your own face object
-function Face(facedetail_value,righteye_value, lefteye_value, mouthh_value, treerootscale) {
+function Face(facedetail_value,righteye_value, lefteye_value, mouthh_value) {
+
+  this.facedetail_value = 2;
+  this.righteye_value = 3;
+  this.lefteye_value = 6;
+  this.mouthh_value = 5;
+
+
   // these are state variables for a face
   // (your variables should be different!)
-  this.detailColour = [204, 136, 17];
-  this.mainColour = [51, 119, 153];
-  this.num_eyes = 2;    // can be either 1 (cyclops) or 2 (two eyes)
-  this.eye_shift = -1;   // range is -10 to 10
-  this.mouth_size = 1;  // range is 0.5 to 8
+  // this.detailColour = [204, 136, 17];
+  // this.mainColour = [51, 119, 153];
+  // this.num_eyes = 2;    // can be either 1 (cyclops) or 2 (two eyes)
+  // this.eye_shift = -1;   // range is -10 to 10
+  // this.mouth_size = 1;  // range is 0.5 to 8
 
-  this.chinColour = [153, 153, 51]
-  this.lipColour = [136, 68, 68]
-  this.eyebrowColour = [119, 85, 17]
+  // this.chinColour = [153, 153, 51]
+  // this.lipColour = [136, 68, 68]
+  // this.eyebrowColour = [119, 85, 17]
+
 
   /*
    * Draw the face with position lists that include:
@@ -52,76 +60,54 @@ function Face(facedetail_value,righteye_value, lefteye_value, mouthh_value, tree
     this.lower_val = 0;
     this.higher_val = 5;
     //let x, y;
-   
-    /////////////// TREE ROOTS ///////////////
-  
-    // tree root left side
-    push();
-    beginShape();
-    strokeWeight(0.1)
-    fill(255,255,255);
-    scale(treerootscale);
-    vertex(3, 1);
-    bezierVertex(5, -5, 1, -4.5, 1, -8);
-    bezierVertex(1, -10, 0, -11, 0, -10);
-    bezierVertex(0, -10, 1, -5, -2.3, -6);
-    bezierVertex(-3, -6, -12, -9, -7, -3.5);
-    bezierVertex(-4, 2, -10, 1, -2, 4);
-    endShape();
-    pop()
-  
-     // tree root left detail
-     push();
-     beginShape();
-     strokeWeight(0.05)
-     fill(255,255,255);
-     scale(treerootscale-0.2);
-     vertex(3, 1);
-     bezierVertex(5, -5, 1, -4.5, 1, -7);
-     bezierVertex(1, -9, 1, -6, 1, -9);
-     bezierVertex(0, -10, 1, -5, -2.3, -6);
-     bezierVertex(-3, -6, -12, -9, -7, -3.5);
-     bezierVertex(-4, 2, -10, 1, -2, 4);
-     endShape();
-     pop()
-  
-    // tree root right
-    push();
-    beginShape();
-    strokeWeight(0.1)
-    fill(255,255,255);
-    rotate(170);
-    scale(treerootscale*0.7);
-    vertex(3, 1);
-    bezierVertex(5, -5, 1, -4.5, 1, -8);
-    bezierVertex(1, -10, 0, -11, 0, -10);
-    bezierVertex(0, -10, 1, -5, -2.3, -6);
-    bezierVertex(-3, -6, -12, -9, -7, -3.5);
-    bezierVertex(-4, 2, -10, 1, -2, 4);
-    endShape();
-    pop()
+
+    this.averageRightEye = segment_average(positions.right_eye);
+    this.averageLeftEye = segment_average(positions.left_eye);
+
+    this.averageNose = segment_average (positions.nose_bridge); // find average nose bridge location
+
+
+    this.R_eyeX = positions.right_eye[0][0];
+    this.R_eyeY = positions.right_eye[0][1];
+
+    this.L_eyeX = positions.left_eye[0][0];
+    this.L_eyeY = positions.left_eye[0][1];
+    
+
+    scale(0.5);
   
   
     scale(0.6);
     angleMode(DEGREES);
   
+  
      /////////////////// FACE BASE CIRC ///////////////////////////
      push();
-     fill(255,255,255); // fill white
+  
+     noFill();
+     stroke(255,192,203);
      strokeWeight(0.3);
+   
+
      this.points2 = 40;
-     stroke(51);
    
      //*** Pheobes Code for circs */
   
      // outer shape
      beginShape();
-     fill(255,255,255);
+
+     ////////////// EXPERIMENTAL //////////
+     //fill(255,255, 255);
+     //ellipse(this.averageRightEye[0]+1.5, this.averageRightEye[1]-3, 1, 1);
+     //ellipse(this.averageLeftEye[0]-2, this.averageLeftEye[1]-3, 1, 1);
+
+     ///////////////////////////////////////
+     //
      for (let i = 0; i < this.points2; i++) {
-       let n = map(noise(i), this.lower_val, this.higher_val, -1, facedetail_value);
+       let n = map(noise(i), this.lower_val, this.higher_val, -1, this.facedetail_value);
        let r = 9 + n;
-       x = r * cos(i * (360 / this.points2));
-       y = r * sin(i * (360 / this.points2));
+       x = r * cos(i * (360 / this.points2)); // how warped in e.g goes skinnier
+       y = r * sin(i * (360 / this.points2)); // how flat it is as a 3d 
        curveVertex(x, y);
      }
      endShape(CLOSE);
@@ -136,11 +122,13 @@ function Face(facedetail_value,righteye_value, lefteye_value, mouthh_value, tree
   
        //first shape
        beginShape();
-       fill(255,255,255);
+       
+       noFill();
+       stroke(255,192,203);
        strokeWeight(0.1)
-       stroke(51);
+     
        for (let i = 0; i < this.points; i++) {
-         let n = map(noise(i), this.lower_val+1, this.higher_val-2, -1, facedetail_value);
+         let n = map(noise(i), this.lower_val+1, this.higher_val-2, -1, this.facedetail_value);
          let r = 10 + n;
          x = r * cos(i * (360 / this.points));
          y = r * sin(i * (360 / this.points));
@@ -152,10 +140,11 @@ function Face(facedetail_value,righteye_value, lefteye_value, mouthh_value, tree
      //most inner circle
      push()
      beginShape();
+     stroke(255,192,203);
      strokeWeight(0.008);
      noFill();
      for (let i = 0; i < this.points; i++) {
-       let n = map(noise(i), this.lower_val+1, this.higher_val-2, -1, facedetail_value);
+       let n = map(noise(i), this.lower_val+1, this.higher_val-2, -1, this.facedetail_value);
        let r = 7 + n;
        x = r * cos(i * (360 / this.points));
        y = r * sin(i * (360 / this.points));
@@ -170,7 +159,7 @@ function Face(facedetail_value,righteye_value, lefteye_value, mouthh_value, tree
     strokeWeight(0.02);
     noFill();
     for (let i = 0; i < this.points; i++) {
-      let n = map(noise(i), this.lower_val+1, this.higher_val-2, -1, facedetail_value);
+      let n = map(noise(i), this.lower_val+1, this.higher_val-2, -1, this.facedetail_value);
       let r = 8 + n;
       x = r * cos(i * (360 / this.points));
       y = r * sin(i * (360 / this.points));
@@ -185,7 +174,7 @@ function Face(facedetail_value,righteye_value, lefteye_value, mouthh_value, tree
        strokeWeight(0.06);
        noFill();
        for (let i = 0; i < this.points; i++) {
-         let n = map(noise(i), this.lower_val+1, this.higher_val-2, -1, facedetail_value);
+         let n = map(noise(i), this.lower_val+1, this.higher_val-2, -1, this.facedetail_value);
          let r = 8.5 + n;
          x = r * cos(i * (360 / this.points));
          y = r * sin(i * (360 / this.points));
@@ -200,7 +189,7 @@ function Face(facedetail_value,righteye_value, lefteye_value, mouthh_value, tree
     noFill();
     strokeWeight(0.02);
     for (let i = 0; i < this.points; i++) {
-      let n = map(noise(i), this.lower_val+1, this.higher_val-2, -1, facedetail_value);
+      let n = map(noise(i), this.lower_val+1, this.higher_val-2, -1, this.facedetail_value);
       let r = 9 + n;
       x = r * cos(i * (360 / this.points));
       y = r * sin(i * (360 / this.points));
@@ -213,7 +202,7 @@ function Face(facedetail_value,righteye_value, lefteye_value, mouthh_value, tree
      beginShape();
      noFill();
      for (let i = 0; i < this.points; i++) {
-       let n = map(noise(i), this.lower_val+1, this.higher_val-2, -1, facedetail_value);
+       let n = map(noise(i), this.lower_val+1, this.higher_val-2, -1, this.facedetail_value);
        let r = 9.5 + n;
        x = r * cos(i * (360 / this.points));
        y = r * sin(i * (360 / this.points));
@@ -223,130 +212,213 @@ function Face(facedetail_value,righteye_value, lefteye_value, mouthh_value, tree
      pop();
       
     ////////////// RIGHT EYE ///////////////////
+
     push();
     noFill();
     strokeWeight(0.3);
-    stroke(51);
+    stroke(255,255, 255);
     this.points3 = 8;
   
     // outer thin shape
       beginShape();
-      fill(255,255,255);
+      
       strokeWeight(0.02); // small detail line weight
       for (let i = 0; i < this.points3; i++) {
-        let n = map(noise(i), this.lower_val+0.5, this.higher_val, -2, righteye_value);
+        let n = map(noise(i), this.lower_val+0.5, this.higher_val, -2, this.righteye_value);
         let r = 3.4 + n;
-        x = r * cos(i * (360 / this.points3));
-        y = r * sin(i * (360 / this.points3));
-        curveVertex(x+2, y-2.5);
+        this.averageRightEye[0] = r * cos(i * (360 / this.points3));
+        this.averageRightEye[1] = r * sin(i * (360 / this.points3));
+        curveVertex(this.averageRightEye[0]+1.5, this.averageRightEye[1]-3);
       }
       endShape(CLOSE);
     
     // main outline (thickest)
     beginShape();
-    fill(255,255,255);
+    //
     strokeWeight(0.1);
     for (let i = 0; i < this.points3; i++) {
-      let n = map(noise(i), this.lower_val+0.5, this.higher_val, -2, righteye_value);
+      let n = map(noise(i), this.lower_val+0.5, this.higher_val, -2, this.righteye_value);
       let r = 3 + n;
-      x = r * cos(i * (360 / this.points3));
-      y = r * sin(i * (360 / this.points3));
-      curveVertex(x+2, y-2.5);
+      this.averageRightEye[0] = r * cos(i * (360 / this.points3));
+      this.averageRightEye[1] = r * sin(i * (360 / this.points3));
+      curveVertex(this.averageRightEye[0]+1.5,  this.averageRightEye[1]-3);
     }
     endShape(CLOSE);
   
     // second outline (2nd thickest)
     beginShape();
-    fill(255,255,255);
+    //
     strokeWeight(0.08);
+    stroke(255,192,203);
     for (let i = 0; i < this.points3; i++) {
-      let n = map(noise(i), this.lower_val+0.5, this.higher_val, -2, righteye_value);
+      let n = map(noise(i), this.lower_val+0.5, this.higher_val, -2, this.righteye_value);
       let r = 2.5 + n;
-      x = r * cos(i * (360 / this.points3));
-      y = r * sin(i * (360 / this.points3));
-      curveVertex(x+2, y-2.5);
+      this.averageRightEye[0] = r * cos(i * (360 / this.points3));
+      this.averageRightEye[1] = r * sin(i * (360 / this.points3));
+      curveVertex(this.averageRightEye[0]+1.5, this.averageRightEye[1]-3);
     }
     endShape(CLOSE);
-  
+
+
+
     pop();
   
+
+
     //////////////////// LEFT EYE ///////////////////////
     push();
     strokeWeight(0.3);
-    stroke(51);
+    stroke(200,100,120);
+    noFill();
   
     this.points4 = 10;
   
     // thinnest outer shape
     beginShape();
-    fill(255,255,255);
+    //
     strokeWeight(0.04);
     for (let i = 0; i <this.points4; i++) {
-      let n = map(noise(i), this.lower_val+0.5, this.higher_val, -2, lefteye_value);
+      let n = map(noise(i), this.lower_val+0.5, this.higher_val, -2, this.lefteye_value);
       let r = 3.5 + n;
-      x = r * cos(i * (360 / this.points4));
-      y = r * sin(i * (360 / this.points4));
-      curveVertex(x-1.5, y-1);
+      this.averageLeftEye[0] = r * cos(i * (360 / this.points4));
+      this.averageLeftEye[1] = r * sin(i * (300 / this.points4));
+      curveVertex(this.averageLeftEye[0]-2,this.averageLeftEye[1]-3);
     }
     endShape(CLOSE);
   
     // main eye shape (thickest)
     beginShape();
-    fill(255,255,255);
+    noFill();
     strokeWeight(0.1);
     for (let i = 0; i < this.points4; i++) {
-      let n = map(noise(i), this.lower_val, this.higher_val, -2, lefteye_value);
+      let n = map(noise(i), this.lower_val, this.higher_val, -2, this.lefteye_value);
       let r = 2.5 + n;
-      x = r * cos(i * (360 / this.points4));
-      y = r * sin(i * (360 / this.points4));
-      curveVertex(x-1.5, y-1);
+      this.averageLeftEye[0] = r * cos(i * (360 / this.points4));
+      this.averageLeftEye[1] = r * sin(i * (300 / this.points4));
+      curveVertex(this.averageLeftEye[0]-2, this.averageLeftEye[1]-3);
     }
     endShape(CLOSE);
   
     // second thinnest line shape
     beginShape();
-    fill(255,255,255);
+    noFill();
     strokeWeight(0.08);
     for (let i = 0; i < this.points4; i++) {
-      let n = map(noise(i), this.lower_val+0.5, this.higher_val, -2, lefteye_value);
+      let n = map(noise(i), this.lower_val+0.5, this.higher_val, -2, this.lefteye_value);
       let r = 2.8 + n;
-      x = r * cos(i * (360 / this.points4));
-      y = r * sin(i * (360 / this.points4));
-      curveVertex(x-1.5, y-1);
+      this.averageLeftEye[0] = r * cos(i * (360 / this.points4));
+      this.averageLeftEye[1] = r * sin(i * (300 / this.points4)); // how squished it is
+      curveVertex(this.averageLeftEye[0]-2, this.averageLeftEye[1]-3);
     }
     endShape(CLOSE);
   
     pop();
     push();
     beginShape();
-    fill(255,255,255);
+    noFill();
     strokeWeight(0.03);
+    stroke(200,100,120);
     for (let i = 0; i < this.points4; i++) {
-      let n = map(noise(i), this.lower_val+0.5, this.higher_val, -2, lefteye_value);
+      let n = map(noise(i), this.lower_val+0.5, this.higher_val, -2, this.lefteye_value);
       let r = 3 + n;
-      x = r * cos(i * (360 / this.points4));
-      y = r * sin(i * (360 / this.points4));
-      curveVertex(x-1.5, y-1);
+      this.averageLeftEye[0] = r * cos(i * (360 / this.points4));
+      this.averageLeftEye[1] = r * sin(i * (300 / this.points4)); // how squished it is
+      curveVertex(this.averageLeftEye[0]-2, this.averageLeftEye[1]-3);
     }
     endShape(CLOSE);
   
     pop();
   
-    ///////////////// MOUTH MOVING /////////////
-    push();
-    fill(255,255,255);
+
+
+
+////////// NOSE ////////////
+
+// if bridge tip is going in a bigger deirection than top of nose and going towards the right, then
+// a second nose is created in the inner for dimension
+
+this.facingrsidestroke = 0.05;
+this.facingfrontstroke= 0;
+
+    this.MidNoseTip = positions.nose_tip[2];
+    this.topofNose = positions.nose_tip[0];
+
+    this.DrawinnerNose;
+
+  if (this.MidNoseTip >= this.topofNose){
+    this.DrawinnerNose = this.facingrsidestroke
+  }
+
+  else{
+    this.DrawinnerNose = this.facingfrontstroke
+  }
+
+// main nose shape
+push();
     strokeWeight(0.3);
-    stroke(51);
+    stroke(200,100,120);
+    noFill();
+  
+    this.pointsnose = 12;
+  
+    beginShape();
+    //
+    strokeWeight (0.1);
+    for (let i = 0; i <this.points4; i++) {
+      let n = map(noise(i), this.lower_val+0.5, this.higher_val, -2, this.lefteye_value);
+      let r = 3 + n;
+      this.averageNose[0] = r * cos(i * (360 / this.pointsnose));
+      this.averageNose[1] = r * sin(i * (250 / this.pointsnose));
+      curveVertex(this.averageNose[0],this.averageNose[1]-1);
+    }
+    endShape(CLOSE);
+  pop();
+
+
+  ///// inner circle for nose 1
+
+  push();
+    strokeWeight(0.3);
+    stroke(200,100,120);
+    noFill();
+  
+    scale(0.7);
+    this.pointsnose = 12;
+  
+    beginShape();
+    //
+    strokeWeight (this.DrawinnerNose);
+    for (let i = 0; i <this.points4; i++) {
+      let n = map(noise(i), this.lower_val+0.5, this.higher_val, -2, this.lefteye_value);
+      let r = 3 + n;
+      this.averageNose[0] = r * cos(i * (360 / this.pointsnose));
+      this.averageNose[1] = r * sin(i * (250 / this.pointsnose));
+      curveVertex(this.averageNose[0],this.averageNose[1]-1);
+    }
+    endShape(CLOSE);
+  pop();
+
+
+    ///////////////// MOUTH MOVING /////////////
+
+// if mouth is closed then squish the mouth and get rid of inner circle
+// if mouth is smiling with teeth then do shocked face squished
+// if half teeth then shoked face
+
+    push();
+    noFill();
+    strokeWeight(0.3);
+    stroke(255,192,203);
   
     this.points5 = 16;
   
    // outer thin detail
    beginShape();
-   fill(255,255,255);
+   noFill();
    strokeWeight(0.02);
    for (let i = 0; i < this.points5; i++) {
-     let n = map(noise(i), this.lower_val, this.higher_val-1, -2, mouthh_value);
-     let r = 3.2 + n;
+     let n = map(noise(i), this.lower_val, this.higher_val-1, -2, this.mouthh_value);
+     let r = 1.8 + n;
      x = r * cos(i * (360 / this.points5));
      y = r * sin(i * (360 / this.points5));
      curveVertex(x, y+3);
@@ -355,10 +427,10 @@ function Face(facedetail_value,righteye_value, lefteye_value, mouthh_value, tree
   
     // main shape outline
     beginShape();
-    fill(255,255,255);
+   // noFill();
     strokeWeight(0.2);
     for (let i = 0; i < this.points5; i++) {
-      let n = map(noise(i), this.lower_val, this.higher_val-1, -2, mouthh_value);
+      let n = map(noise(i), this.lower_val, this.higher_val-1, -2, this.mouthh_value);
       let r = 3 + n;
       x = r * cos(i * (360 / this.points5));
       y = r * sin(i * (360 / this.points5));
@@ -368,19 +440,29 @@ function Face(facedetail_value,righteye_value, lefteye_value, mouthh_value, tree
   
      // thinner shape outline
     beginShape();
-    fill(255,255,255);
+   // noFill();
     strokeWeight(0.1);
     for (let i = 0; i < this.points5; i++) {
-      let n = map(noise(i), this.lower_val, this.higher_val-1, -2, mouthh_value);
+      let n = map(noise(i), this.lower_val, this.higher_val-1, -2, this.mouthh_value);
       let r = 2.5 + n;
       x = r * cos(i * (360 / this.points5));
       y = r * sin(i * (360 / this.points5));
       curveVertex(x, y+3);
     }
     endShape(CLOSE);
+
+    
+
   
     pop();
+
+
+    
+
+ 
+
   
+    
   //   console.log()
   //   // head
   //   ellipseMode(CENTER);
@@ -470,17 +552,27 @@ function Face(facedetail_value,righteye_value, lefteye_value, mouthh_value, tree
 
   /* set internal properties based on list numbers 0-100 */
   this.setProperties = function(settings) {
-    this.num_eyes = int(map(settings[0], 0, 100, 1, 2));
-    this.eye_shift = map(settings[1], 0, 100, -2, 2);
-    this.mouth_size = map(settings[2], 0, 100, 0.5, 8);
+    // this.num_eyes = int(map(settings[0], 0, 100, 1, 2));
+    // this.eye_shift = map(settings[1], 0, 100, -2, 2);
+    // this.mouth_size = map(settings[2], 0, 100, 0.5, 8);
+    
+    this.facedetail_value = map(settings[3], 0, 100, 0.5, 7);
+    this.righteye_value= map(settings[4], 0, 100, 0, 5);
+    this.lefteye_value = map(settings[5], 0, 100, 0, 10);
+    this.mouthh_value = map(settings[6], 0, 100, 0, 5);
   }
 
   /* get internal properties as list of numbers 0-100 */
   this.getProperties = function() {
     let settings = new Array(3);
-    settings[0] = map(this.num_eyes, 1, 2, 0, 100);
-    settings[1] = map(this.eye_shift, -2, 2, 0, 100);
-    settings[2] = map(this.mouth_size, 0.5, 8, 0, 100);
+    // settings[0] = map(this.num_eyes, 1, 2, 0, 100);
+    // settings[1] = map(this.eye_shift, -2, 2, 0, 100);
+    // settings[2] = map(this.mouth_size, 0.5, 8, 0, 100);
+
+    settings[3] = map(this.facedetail_value, 0.5, 7, 0, 100);
+    settings[4] = map(this.righteye_value, 0, 5, 0, 100);
+    settings[5] = map(this.lefteye_value, 0, 10, 0, 100);
+    settings[6] = map(this.mouthh_value, 0, 5, 0, 100);
     return settings;
   }
 }
